d28a7a137845bb080fef1f826b46c8330d135fb6
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-10-18 16:55:34 +0800

    Add files via upload

---
+++ src/main/java/net/codejava/ProductRepository.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+    Page<Product> findAllByNameContainingOrBrandContainingOrMadeinContaining(String name, String brand, String madein, Pageable pageable);
+
+}
